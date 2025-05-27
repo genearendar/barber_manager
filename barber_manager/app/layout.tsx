7219b910d6e2d576e586fa9a4539068c5fbb5c81
Gene Arendarenko <Genearendar@gmail.com>
2025-05-27 05:51:17 +0000

    Romove some tutorial sections

--- barber_manager/app/layout.tsx
+++ barber_manager/app/layout.tsx
@@ -14,8 +14,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Barbershop Queue Management App",
+  description: "Barbershop Queue Management App",
 };
 
 const geistSans = Geist({
