9b16fe03b6c6488c1d4de1e262cf9c9fc4058e48
Gene Arendarenko <Genearendar@gmail.com>
2025-05-31 12:29:50 +0000

    Reduce the size of queue cards

--- barber_manager/components/custom/queue-container.tsx
+++ barber_manager/components/custom/queue-container.tsx
@@ -23,16 +23,19 @@ export default function QueueContainer({
     return (
       <div
         key={entry.id}
-        className="flex flex-col gap-6 bg-white/80 backdrop-blur-sm rounded-2xl p-12 shadow-lg border border-gray-200"
+        className="flex flex-col gap-2 bg-white/80 backdrop-blur-sm rounded-2xl p-6 shadow-lg border border-gray-200"
       >
         <div className="flex justify-between">
-          <div className="w-fit bg-gradient-to-r from-blue-500 to-blue-600 text-white text-sm font-bold px-4 py-2 rounded-full">
-            #{index + 1}
+          <div className="flex gap-4 items-center">
+            <div className="w-fit bg-gradient-to-r from-blue-500 to-blue-600 text-white text-sm font-bold px-4 py-1 rounded-full">
+              #{index + 1}
+            </div>
+            <h3 className="text-xl font-bold text-gray-900">{entry.name}</h3>
           </div>
+
           <span className="text-2xl">✂️</span>
         </div>
 
-        <h3 className="text-xl font-bold text-gray-900">{entry.name}</h3>
         {/* <p>{entry.status}</p> */}
         <div className="flex justify-between items-center">
           <div className="flex items-center">
@@ -47,7 +50,7 @@ export default function QueueContainer({
     );
   });
   return (
-    <div className="flex flex-col gap-2 max-w-2xl">
+    <div className="flex flex-col gap-2 max-w-xl">
       <div className="flex justify-between mb-12">
         <div className="flex items-center">
           <div className="bg-gradient-to-r from-red-500 to-red-600 p-3 rounded-xl mr-4">
