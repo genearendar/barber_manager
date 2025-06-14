0186ceddff4ed2a3a488fc88de4629daadff8b2f
Gene Arendarenko <Genearendar@gmail.com>
2025-06-14 06:53:45 +0000

    Update onboarding form

--- barber_manager/app/onboarding/page.tsx
+++ barber_manager/app/onboarding/page.tsx
@@ -7,15 +7,13 @@ export default function OnboardingPage() {
 
   return (
     <div className="max-w-md mx-auto mt-12 p-4 border rounded-xl shadow">
-      <OnboardingForm/>
+      <OnboardingForm />
       <h1 className="text-2xl font-bold mb-4">
         Welcome to MyClipmate, mate! 👋
       </h1>
       <p className="mb-6 text-gray-600">
         Let's get your barbershop set up in just a minute.
       </p>
-
-      
     </div>
   );
 }
