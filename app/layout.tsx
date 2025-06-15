1768bdc2419c0504e0d9677cbad7b73270ea5895
Gene Arendarenko <Genearendar@gmail.com>
2025-06-15 10:40:55 +0000

    Add Analytics component to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,7 @@ export default async function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
